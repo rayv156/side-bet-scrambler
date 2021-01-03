a65f5f49d987f080850e2ccb86dd19ada266bc35
Ray Velasquez <rayvelasquez@Rays-MacBook-Pro.local>
2021-01-03 11:42:56 -0600

    fixed login btn

--- views/auth/login.jsx
+++ views/auth/login.jsx
@@ -6,16 +6,16 @@ class Login extends React.Component {
     return (
       <Index title="Side Bet Scrambler">
         <div className="form-container">
-          <img src="https://i.imgur.com/CKea2dD.png?1" style={{width: 50, backgroundColor: 'green', margin: '10px 0px'}}></img>
-        <form action="/" method="post">
+          <img src="https://i.imgur.com/CKea2dD.png?1" style={{width: 50, backgroundColor: 'rgb(74,126,76)', margin: '10px 0px'}}></img>
+        <form action="/" method="post" style={{justifyContent: 'center'}}>
           <div className="form-group">
           <input type="text" className="form-control" name="username" placeholder="username" style={{border: 'solid green'}}/>
           </div>
           <div className="form-group">
           <input type="password" className="form-control" name="password" placeholder="password" style={{border: 'solid green'}} />
           </div>
-          <div className="form-group" style={{marginLeft: 60}}>
-          <input type="submit" className="btn btn-dark" id="login" value="Log in" />
+          <div id="login">
+          <input type="submit" className="btn btn-success login" value="Log in" />
           </div>
         </form>
         <a href="/signup"><button type="button" class="btn btn-primary" id="signup">Sign Up</button></a>
